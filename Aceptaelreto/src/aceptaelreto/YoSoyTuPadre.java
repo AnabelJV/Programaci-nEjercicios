84c6016453185e87d140e4e4eff4805d6dcd9233
anavil <anavil@a1414>
2018-01-16 17:20:38 +0100

    Empezando programa

---
+++ Aceptaelreto/src/aceptaelreto/YoSoyTuPadre.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aceptaelreto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anavil
+ */
+public class YoSoyTuPadre {
+    
+     public static void main(String[] args) {
+        
+         Scanner sc = new Scanner(System.in);
+         
+         System.out.println("¿Cuantos casos de prueba?");
+         int pruebas = sc.nextInt();
+         
+         for ( int i=0; i<=pruebas; i++ )
+         {
+             
+         }
+         
+     }
+       
+    
+}
